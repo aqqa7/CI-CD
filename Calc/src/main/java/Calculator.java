1affc8f1f4ecbd4d628b85257880bfd8c8ef45ba
Patricio Flores <aqqa77770@gmail.com>
2023-10-10 15:43:14 -0300

    calc methods added to base_c

--- Calc/src/main/java/Calculator.java
+++ Calc/src/main/java/Calculator.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +11,23 @@
  * @author AZ
  */
 public class Calculator {
+    Scanner in = new Scanner(System.in);
+    
+    public void sum(){
+        System.out.println("Ingrese los números a sumar");
+        int n1 = in.nextInt();
+        int n2 = in.nextInt();
+        int res = n1+n2;
+        System.out.println("El resultado es :"+res);
+    }
+    
+    public void subs(){
+      System.out.println("Ingrese los números a restar");
+        int n1 = in.nextInt();
+        int n2 = in.nextInt();
+        int res = n1-n2;
+        System.out.println("El resultado es :"+res);
+    }
     
 }
+
